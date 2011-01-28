4c1811940e873a0a7b52e15369761a33bc4ca38a
Nick Markwell <nick@duckinator.net>
2011-01-28 00:04:17 -0500

    Misc cleanup + simple testing

--- hlogic.hs
+++ hlogic.hs
@@ -1,6 +1,7 @@
 module Hlogic where
 import qualified Prelude
-import Prelude(Bool(True, False))
+import Prelude(Bool(True, False), (==), (++))
+import IO
 
 -- NOT a
 not True   = False
@@ -11,48 +12,88 @@ True  `and` True  = True
 _     `and` _     = False
 
 -- a OR b
---False `or`  False = False
---_     `or`  _     = True
-a `or`   b  = (not a) `nand` (not b)
-
--- a NOR b
-a `nor`  b  = not (a `or`  b)
+a `or`   b  = not a ~&& not b
 
 -- a NAND b
-a `nand` b  = not (a `and` b)
+a `nand` b  = not (a && b)
+
+-- a NOR b
+a `nor`  b  = not (a || b)
 
 -- p --> q (p implies q)
-p --> q = p `and` (not q)
--- True --> False = False
--- _    --> _     = True
+p --> q = p && not q
 
 -- p <-- q (p is implied by q)
-p <-- q = (not p) `and` q
--- False <-- True = False
--- _     <-- _    = True
+p <-- q = not p && q
 
 -- p -/> q (p does not imply by q)
 p -/> q = not (p --> q)
--- True -/> False = True
--- _    -/> _     = False
 
 -- p </- q (p is not implied by q)
 p </- q = not (p <-- q)
--- False </- True = True
--- _     </- _    = False
 
 -- a XOR b
-a `xor`  b = (a `and` not b) `or` (not a `and` b)
-a `xnor` b = not (a `xor` b)
+a `xor`  b = (a && not b) || (not a && b)
+a `xnor` b = not (a ^ b)
 
 --------------
 -- Aliases! --
 --------------
 
-~ a = not a
+-- ~ a = not a
 
 a && b = a `and` b
 a || b = a `or`  b
-a ^  b = a `xor` b
 
+a ~&& b = a `nand` b
+a ~|| b = a `nor`  b
+
+a  ^ b = a `xor` b
+a ~^ b = a `xnor` b
+
+a ! b  = not a
+
+
+
+test name result expected =
+  if result == expected
+    then do
+           Prelude.putStrLn ("Test " ++ Prelude.show name ++ " passed.")
+           Prelude.return True
+    else do
+           Prelude.putStrLn ("Test " ++ Prelude.show name ++ " failed:\n"      ++
+                             "    Expected: " ++ Prelude.show expected ++ "\n" ++
+                             "    Got:      " ++ Prelude.show result)
+           Prelude.return False
+
+main = do
+  test "AND   (1)" (False &&  False) False
+  test "AND   (2)" (False &&  True)  False
+  test "AND   (3)" (True  &&  False) False
+  test "AND   (4)" (True  &&  True)  True
+  
+  test "OR    (1)" (False ||  False) False
+  test "OR    (2)" (False ||  True)  True
+  test "OR    (3)" (True  ||  False) True
+  test "OR    (4)" (True  ||  True)  True
+  
+  test "NAND  (1)" (False ~&& False) True
+  test "NAND  (2)" (False ~&& True)  True
+  test "NAND  (3)" (True  ~&& False) True
+  test "NAND  (4)" (True  ~&& True)  False
+  
+  test "NOR   (1)" (False ~|| False) True
+  test "NOR   (2)" (False ~|| True)  False
+  test "NOR   (3)" (True  ~|| False) False
+  test "NOR   (4)" (True  ~|| True)  False
+  
+  test "XOR   (1)" (False  ^  False) False
+  test "XOR   (2)" (False  ^  True)  True
+  test "XOR   (3)" (True   ^  False) True
+  test "XOR   (4)" (True   ^  True)  False
+  
+  test "XNOR  (1)" (False ~^  False) True
+  test "XNOR  (2)" (False ~^  True)  False
+  test "XNOR  (3)" (True  ~^  False) False
+  test "XNOR  (4)" (True  ~^  True)  True
 
