121dd1f92aa41153b05ebc5217798b83dd135e87
Nick Markwell <nick@duckinator.net>
2011-01-27 20:13:42 -0500

    Derp. Fixed.

--- hlogic.hs
+++ hlogic.hs
@@ -23,23 +23,23 @@ a `nand` b  = not (a `and` b)
 
 -- p --> q (p implies q)
 p --> q = p `and` (not q)
---True --> False = False
---_    --> _     = True
+-- True --> False = False
+-- _    --> _     = True
 
 -- p <-- q (p is implied by q)
 p <-- q = (not p) `and` q
---False <-- True = False
---_     <-- _    = True
+-- False <-- True = False
+-- _     <-- _    = True
 
 -- p -/> q (p does not imply by q)
 p -/> q = not (p --> q)
---True -/> False = True
---_    -/> _     = False
+-- True -/> False = True
+-- _    -/> _     = False
 
 -- p </- q (p is not implied by q)
 p </- q = not (p <-- q)
---False </- True = True
---_     </- _    = False
+-- False </- True = True
+-- _     </- _    = False
 
 -- a XOR b
 a `xor`  b = (a `and` not b) `or` (not a `and` b)
@@ -51,8 +51,8 @@ a `xnor` b = not (a `xor` b)
 
 ~ a = not a
 
-a `&&` b = a `and` b
-a `||` b = a `or`  b
-a `^`  b = a `xor` b
+a && b = a `and` b
+a || b = a `or`  b
+a ^  b = a `xor` b
 
 
